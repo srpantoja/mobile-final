52c684017c4d502e7ac03182268ccc5fd0da8b0c
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-10 20:45:41 -0300

    login screen

--- app/src/main/java/com/example/trab_final/view/Signup.kt
+++ app/src/main/java/com/example/trab_final/view/Signup.kt
@@ -1,11 +1,11 @@
-package com.example.trab_final
+package com.example.trab_final.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
