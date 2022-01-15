7f097ae75b942a306dbdfdf91a4e9f8e3b9f1149
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-15 18:27:36 -0300

    orders view

---
+++ app/src/main/java/com/example/trab_final/view/DeliveryListOrders.kt
@@ -0,0 +1,12 @@
+package com.example.trab_final.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trab_final.R
+
+class DeliveryListOrders : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_list_orders)
+    }
+}
