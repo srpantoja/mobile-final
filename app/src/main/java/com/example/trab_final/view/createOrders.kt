5bf6d29c79daba924a161448f5669ccdec8b1ea1
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-17 19:07:35 -0300

    bugfix

--- app/src/main/java/com/example/trab_final/view/createOrders.kt
+++ app/src/main/java/com/example/trab_final/view/createOrders.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trab_final.R
 
-class CadPedidos : AppCompatActivity() {
+class createOrders : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_cadPedidos)
+        setContentView(R.layout.activity_create_orders)
     }
 }
