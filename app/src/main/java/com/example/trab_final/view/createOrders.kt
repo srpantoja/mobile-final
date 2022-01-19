58205f7e6e991b1ddfd299e7b689b471a449bac3
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-19 20:28:19 -0300

    create pedido ok

--- app/src/main/java/com/example/trab_final/view/createOrders.kt
+++ app/src/main/java/com/example/trab_final/view/createOrders.kt
@@ -1,12 +1,11 @@
 package com.example.trab_final.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
 import com.example.trab_final.R
+import com.example.trab_final.models.Orders
 import com.example.trab_final.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -18,13 +17,14 @@ class createOrders : AppCompatActivity() {
     private lateinit var ruaField : EditText
     private lateinit var numeroField : EditText
     private lateinit var codPedidoField : EditText
-    private lateinit var entregadorField : Spinner
+    private lateinit var entregadorField : EditText
     private lateinit var addButton : Button
     private lateinit var database : DatabaseReference
     private lateinit var auth : FirebaseAuth
     private lateinit var _userList : ArrayList<User>
     private lateinit var _deliveryList : ArrayList<User>
     private lateinit var _currentUser : User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_orders)
@@ -55,13 +55,35 @@ class createOrders : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {}
         })
 
-        for( delivery in _userList){
-            if(delivery.companyId == _currentUser.companyId)
-                _deliveryList.add(delivery)
+        addButton.setOnClickListener {
+            var entregador = ""
+            for( x in _userList){
+                if(x.email.toString() == entregadorField.text.toString())
+                    entregador = x.uId.toString()
+            }
+            if(entregador != ""){
+            addOrders(
+                nameClienteField.text.toString(),
+                bairroField.text.toString(),
+                ruaField.text.toString(),
+                numeroField.text.toString(),
+                codPedidoField.text.toString(),
+                entregador,
+                _currentUser.companyId.toString()
+            )
+            }
+            else{
+                Toast.makeText(this, "User does not exist", Toast.LENGTH_LONG).show()
+            }
         }
+    }
 
-        val adapterSpinner = ArrayAdapter(this, android.R.layout.simple_list_item_1, _deliveryList)
-        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        entregadorField.adapter = adapterSpinner
+    private fun addOrders(name: String?, street: String?, district: String?, number: String?, codProduct: String?, deliveryId: String?, companyId: String?){
+        database = FirebaseDatabase.getInstance().getReference()
+
+        database.child("orders").push().setValue(Orders(name, street,district, number, codProduct, deliveryId, companyId))
+
+        val intent = Intent(this@createOrders, funcionarioListOrders::class.java)
+        startActivity(intent)
     }
 }
