2f44a79eeb052cddaab720b0f13fa8fb83054929
Jean Pantoja <39068801+srpantoja@users.noreply.github.com>
2022-01-19 18:28:18 -0300

    add funcionarios

--- app/src/main/java/com/example/trab_final/view/fragmentsEmpresa/FuncionarioFragment.kt
+++ app/src/main/java/com/example/trab_final/view/fragmentsEmpresa/FuncionarioFragment.kt
@@ -1,6 +1,7 @@
 package com.example.trab_final.view.fragmentsEmpresa
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,22 +10,59 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import com.example.trab_final.R
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.example.trab_final.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 
 class FuncionarioFragment : Fragment() {
 
+    private lateinit var addFuncionarioButton: Button
+    private lateinit var emailField: EditText
+    private lateinit var database: DatabaseReference
+    private lateinit var _userList: ArrayList<User>
+    private lateinit var auth: FirebaseAuth
+    private lateinit var _user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_funcionario, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_funcionario, container, false)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference().child("user")
+        Log.v("view", view.toString())
+        val companyId = auth.currentUser?.uid
+        _userList = ArrayList<User>()
+
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (postSnapshop in snapshot.children) {
+                    var currentUser = postSnapshop.getValue(User::class.java)
+                    _userList.add(currentUser!!)
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
+        addFuncionarioButton = view.findViewById(R.id.btn_addFunc)
+        emailField = view.findViewById(R.id.textEmail)
+        Log.v("database", database.toString())
+
+        addFuncionarioButton.setOnClickListener {
+            val email = emailField.text.toString()
+
+            for(user in _userList){
+                if(user.email == email)
+                    _user = user
+            }
+            _user.companyId = companyId
+            database.child(_user.uId.toString()).setValue(_user)
+        }
+
+        return view
     }
 
 }
--- app/src/main/java/com/example/trab_final/view/funcionarioListOrders.kt
+++ app/src/main/java/com/example/trab_final/view/funcionarioListOrders.kt
@@ -1,12 +1,22 @@
 package com.example.trab_final.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.trab_final.R
 
 class funcionarioListOrders : AppCompatActivity() {
+    private lateinit var addButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_funcionario_list_orders)
+
+        addButton = findViewById(R.id.addEntrega)
+
+        addButton.setOnClickListener {
+            val intent = Intent(this@funcionarioListOrders, createOrders::class.java)
+            startActivity(intent)
+        }
     }
 }
